9b48b52bbdecc06971ec2d15977c98adef76d3ad
Tristenh <tristenhobba1@gmail.com>
2023-09-28 22:24:08 +1000

    npm installed, add comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// configure workbox plugins for a service worker and manifest file.
 
 module.exports = (env, argv) => {
   const injectManifest = new InjectManifest({
@@ -37,6 +36,8 @@ module.exports = (env, argv) => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
+
+    //  CSS loaders and babel to webpack.
     plugins: [
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
